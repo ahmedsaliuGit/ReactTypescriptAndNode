840b85fcd208d41f9d0345af8fe98d9a585ce4db
ahmedsaliuGit <saliu.lito@gmail.com>
2022-02-07 14:31:01 +0100

    Fix react router dom for try-react

--- chap4/try-react/src/App.tsx
+++ chap4/try-react/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import AnotherScreen from "./AnotherScreen";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/anotherscreen" component={AnotherScreen} />
+        </Switch>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- chap4/try-react/src/index.tsx
+++ chap4/try-react/src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Application from "./Application";
+import { BrowserRouter } from "react-router-dom";
+// import Application from "./Application";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Application />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
